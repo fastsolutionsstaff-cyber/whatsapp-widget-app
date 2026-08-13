b346458bee8426d2b9ccc534b4ef1bc1ff1c92e1
fastsolutionsstaff-cyber <fastsolutionsstaff@gmail.com>
2026-08-13 15:53:07 +0500

    test with limit 1

--- app/routes/proxy.track-click.jsx
+++ app/routes/proxy.track-click.jsx
@@ -2,7 +2,7 @@ import { json } from "@remix-run/node";
 import { authenticate } from "../shopify.server";
 import prisma from "../db.server";
 
-const FREE_CLICK_LIMIT = 1;
+const FREE_CLICK_LIMIT = 2;
 
 export async function loader({ request }) {
   return handleTracking(request);
