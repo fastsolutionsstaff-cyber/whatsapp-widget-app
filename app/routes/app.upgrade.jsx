b5a8ec7c5712cc8bc969a602773ef4d44431c707
fastsolutionsstaff-cyber <fastsolutionsstaff@gmail.com>
2026-08-15 11:15:04 +0500

    fix Shopify upgrade redirect

--- app/routes/app.upgrade.jsx
+++ app/routes/app.upgrade.jsx
@@ -1,21 +1,15 @@
+import { redirect } from "@remix-run/node";
 import { authenticate } from "../shopify.server";
 
 export async function loader({ request }) {
-  const { session, redirect } =
-    await authenticate.admin(request);
+  const { session } = await authenticate.admin(request);
 
-  const shopHandle = session.shop.replace(
-    ".myshopify.com",
-    ""
-  );
+  const shop = session.shop.replace(".myshopify.com", "");
 
-  const appHandle = "widget-whatsapp";
+  const url =
+    `https://admin.shopify.com/store/${shop}/charges`;
 
-  const pricingUrl =
-    `https://admin.shopify.com/store/${shopHandle}` +
-    `/charges/${appHandle}/pricing_plans`;
-
-  return redirect(pricingUrl, {
+  return redirect(url, {
     target: "_top",
   });
 }
