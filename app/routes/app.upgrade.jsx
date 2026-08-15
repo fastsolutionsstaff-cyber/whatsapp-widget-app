4e78c583b25b223a5739e550afb461b3c96a3cfb
fastsolutionsstaff-cyber <fastsolutionsstaff@gmail.com>
2026-08-15 11:46:45 +0500

    fix Shopify App Pricing redirect

--- app/routes/app.upgrade.jsx
+++ app/routes/app.upgrade.jsx
@@ -1,15 +1,14 @@
-import { redirect } from "@remix-run/node";
 import { authenticate } from "../shopify.server";
 
 export async function loader({ request }) {
-  const { session } = await authenticate.admin(request);
+  const { session, redirect } = await authenticate.admin(request);
 
-  const shop = session.shop.replace(".myshopify.com", "");
+  const storeHandle = session.shop.replace(".myshopify.com", "");
 
-  const appHandle = "widget-whatsapp-1";
+  const appHandle = "widget-whatsapp";
 
   const pricingUrl =
-    `https://admin.shopify.com/store/${shop}` +
+    `https://admin.shopify.com/store/${storeHandle}` +
     `/charges/${appHandle}/pricing_plans`;
 
   return redirect(pricingUrl, {
